e1fed44a368fc3694a378ced16ea9a9a51860636
J Kangas <jussi.v.kangas@gmail.com>
2021-07-06 10:32:05 +0200

    VisualBoard

---
+++ Assets/Scripts/VisualBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualBoard : MonoBehaviour
+{
+
+    public VisualCell[] cells = new VisualCell[9];
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for(int i=1; i<10; i++){
+            var cell = GameObject.Find("Cell"+i.ToString());
+            cells[i-1].Object=cell;
+            cells[i-1].CellNumber=i;
+        }
+    }
+
+}
---
+++ Assets/Scripts/VisualCell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualCell : MonoBehaviour
+{
+
+    public int CellNumber {get; set;}
+    public GameObject Object {get; set;}
+
+    void ChangeAppereance(){
+
+    }
+
+    void PrintCell(){
+        print("This is cell number: "+CellNumber.ToString());
+    }
+    
+    void SpawnPiece(string path){
+        GameObject prefab = (GameObject) Resources.Load(path);
+        Vector3 pos = Object.transform.position+ new Vector3(0f,0f,0.2f);
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+}
