6dde5540584234294304808768c0743a8a790d9f
J Kangas <jussi.v.kangas@gmail.com>
2021-07-07 16:16:18 +0200

    materials

--- Assets/Scripts/VisualBoard.cs
+++ Assets/Scripts/VisualBoard.cs
@@ -7,9 +7,12 @@ public class VisualBoard : MonoBehaviour
 
     public GameObject[] cells = new GameObject[9];
     public Material cellMaterial;
+    public Material barMaterial;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        cellMaterial = (Material) Resources.Load("Materials/Grass");
+        barMaterial = (Material) Resources.Load("Materials/Bark/Bark07");
         for(int i=1; i<10; i++){
             var cell = GameObject.Find("Cell"+i.ToString());
             cell.tag = "Cell";
@@ -17,6 +20,10 @@ public class VisualBoard : MonoBehaviour
             cell.GetComponent<VisualCell>().CellNumber=i-1;
             cell.GetComponent<MeshRenderer>().material=cellMaterial;
             cells[i-1]=cell;
+        }
+        for(int i=1; i<5; i++){
+            var cell = GameObject.Find("Bar"+i.ToString());
+            cell.GetComponent<MeshRenderer>().material=barMaterial;
         }
     }
 
